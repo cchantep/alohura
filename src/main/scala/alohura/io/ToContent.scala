d81f5f22d5ace0f9e9aa19ff3e49af7c12b942f6
YoEight <yo.eight@gmail.com>
2013-04-15 13:41:24 +0200

    add ToContent and Method

---
+++ src/main/scala/alohura/io/ToContent.scala
@@ -0,0 +1,15 @@
+package alohura.io
+
+import dispatch._
+
+trait ToContent[A] extends (Res ⇒ A)
+
+object ToContent {
+  implicit val stringToContent = new ToContent[String] {
+    def apply(r: Res) = as.String(r)
+  }
+
+  implicit val bytesToContent = new ToContent[Array[Byte]] {
+    def apply(r: Res) = as.Bytes(r)
+  }
+}
